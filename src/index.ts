b8802279d374b469b3a43c1c64d82628a6d21e3c
Alexander Schwartzberg <aeksco@gmail.com>
2020-04-05 21:04:26 -0400

    Updated tsconfig.json out directory, src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-export function goFish() {
-  return false;
-}
+export = {
+  name: "ExpressJS + MongoDB API",
+  async write() {
+    await this.composeWith("./api_base");
+    await this.composeWith("./api_environment");
+    // await this.composeWith("./api_controller");
+    // await this.composeWith("./api_model");
+    // await this.composeWith("./api_routes");
+    // await this.composeWith("./api_resource_spec");
+  },
+};
+
+// export const name: string = "ExpressJS + MongoDB API";
+
+// export async function write() {
+//   await this.composeWith("./api_base");
+//   await this.composeWith("./api_environment");
+//   // await this.composeWith("./api_controller");
+//   // await this.composeWith("./api_model");
+//   // await this.composeWith("./api_routes");
+//   // await this.composeWith("./api_resource_spec");
+// }
