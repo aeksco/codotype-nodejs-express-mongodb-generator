6f0eb2296a15ff932cf6bfe36c0b66c78ac22b7f
Alexander Schwartzberg <aeksco@gmail.com>
2020-04-05 20:57:05 -0400

    Updated api_base, api_environment generator templates

--- generator/api_base/index.js
+++ generator/api_base/index.js
@@ -1,20 +1,24 @@
 module.exports = {
-  name: 'NodeExpressBase',
-  async write ({ blueprint }) {
-
+  name: "NodeExpressBase",
+  async write({ project }) {
     // Copies server base code
-    await this.copyDir({ src: '', dest: '' })
+    await this.copyDir({ src: "", dest: "" });
 
-    const userSchema = blueprint.schemas.find(s => s.identifier === 'user')
-    const inlineDeconstruction = userSchema.attributes.map(r => r.identifier).join(', ')
+    // Isolates UserSchema + build inlineDeconstruction
+    // const userSchema = project.schemas.find((s) => s.identifier === "user");
+    // const inlineDeconstruction = userSchema.attributes
+    //   .map((r) => r.identifier)
+    //   .join(", ");
 
-    await this.renderComponent({
-      src: 'src/api/auth/auth.controller.js',
-      dest: 'src/api/auth/auth.controller.js',
-      data: { inlineDeconstruction }
-    })
+    // Renders auth controller
+    // await this.renderComponent({
+    //   src: "src/api/auth/auth.controller.js",
+    //   dest: "src/api/auth/auth.controller.js",
+    //   data: { inlineDeconstruction },
+    // });
 
-    await this.renderComponent({ src: 'LICENSE', dest: 'LICENSE' })
-    await this.renderComponent({ src: 'package.json', dest: 'package.json' })
-  }
-}
+    // Writes LICENSE + package.json
+    await this.renderComponent({ src: "LICENSE", dest: "LICENSE" });
+    await this.renderComponent({ src: "package.json", dest: "package.json" });
+  },
+};
--- generator/api_controller/index.js
+++ generator/api_controller/index.js
@@ -1,43 +1,49 @@
 module.exports = {
-  name: 'NodeExpressController',
-  async forEachSchema({ blueprint, configuration, schema }) {
-
+  name: "NodeExpressController",
+  async forEachSchema({ project, schema }) {
     // Pulls `generate_api_doc` from configuration.options
     // Used to conditionally generate APIDoc headers
-    const { generate_api_doc } = configuration.documentation
+    const { generate_api_doc } = project.configuration.documentation;
 
     // Pulls schema api_actions
-    let schemaApiActions = []
-    if (configuration.api_actions && configuration.api_actions[schema.identifier]) {
-      schemaApiActions = configuration.api_actions[schema.identifier]
+    let schemaApiActions = [];
+    if (
+      project.configuration.api_actions &&
+      project.configuration.api_actions[schema.identifier]
+    ) {
+      schemaApiActions = project.configuration.api_actions[schema.identifier];
     }
 
     // Defines the schema-specific destination
-    let resourceDest = 'src/api/' + schema.identifier
+    let resourceDest = "src/api/" + schema.identifier;
 
     // Ensures the presence of the directory
-    await this.ensureDir(resourceDest)
+    await this.ensureDir(resourceDest);
 
     // Deconstructs schema attributes
-    let defaultModel = this.buildDefault({ schema, schemas: blueprint.schemas })
+    let defaultModel = this.buildDefault({ schema, schemas: project.schemas });
     // TODO - might be helpful to abstract into util, or parent generator?
-    let inlineDeconstruction = Object.keys(defaultModel).join(', ')
-    let objectKeys = Object.keys(defaultModel)
+    let inlineDeconstruction = Object.keys(defaultModel).join(", ");
+    let objectKeys = Object.keys(defaultModel);
 
     // src/api/resource/resource.controller.js
     await this.renderComponent({
-      src: 'resource.controller.js',
-      dest: resourceDest + '/' + schema.identifier + '.controller.js',
-      data: { schema, generate_api_doc, schemaApiActions, inlineDeconstruction, objectKeys }
+      src: "resource.controller.js",
+      dest: resourceDest + "/" + schema.identifier + ".controller.js",
+      data: {
+        schema,
+        generate_api_doc,
+        schemaApiActions,
+        inlineDeconstruction,
+        objectKeys,
+      },
     });
 
     // src/api/resource/index.js
     await this.renderComponent({
-      src: 'resource.router.js',
-      dest: resourceDest + '/index.js',
-      data: { schema, schemaApiActions }
+      src: "resource.router.js",
+      dest: resourceDest + "/index.js",
+      data: { schema, schemaApiActions },
     });
-
-  }
-
+  },
 };
